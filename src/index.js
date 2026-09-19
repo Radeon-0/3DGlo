49af07380036921ecf45c263405aa3ede3fce4c8
Radion0 <radion.glushkov@mail.ru>
2026-09-19 23:29:10 +0700

    ДЗ

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import timer  from './modules/timer';
-
+import menu from './modules/menu'
+import modal from './modules/modal';
 timer('29 september 2026');
+menu();
+modal();
---
+++ src/modules/modal.js
@@ -0,0 +1,39 @@
+const modal = () => {
+    const modal = document.querySelector('.popup');
+    const buttons = document.querySelectorAll('.popup-btn');
+    const closeeBtn = modal.querySelector('.popup-close');
+    let opacity = 0;
+    let position = -50;
+
+    const animate = () => {
+        opacity += 0.02;
+        position += 1;
+
+        modal.style.opacity = opacity;
+        modal.style.transform = `translateY(${position}px)`;
+
+        if (opacity < 1) {
+            requestAnimationFrame(animate);
+        }
+    };
+
+    buttons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.style.display = "block";
+
+            if (window.innerWidth < 768) {
+                modal.style.opacity = '1';
+                modal.style.transform = 'translateY(0)';
+                return;
+            }
+
+            animate();
+        })
+    });
+
+    closeeBtn.addEventListener("click", () => {
+        modal.style.display = "none";
+    })
+
+}
+export default modal
