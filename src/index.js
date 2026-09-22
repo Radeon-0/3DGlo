36420477833df9072a62792baa57816519d564a7
Radion0 <radion.glushkov@mail.ru>
2026-09-22 17:16:45 +0700

    ДЗ

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import timer  from './modules/timer';
 import menu from './modules/menu'
 import modal from './modules/modal';
+import validForm from './modules/validForm';
 timer('29 september 2026');
 menu();
 modal();
+validForm();
